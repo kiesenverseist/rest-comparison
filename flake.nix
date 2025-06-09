694810f7cacd61f2076addb84f08451c30699bcd
Ibrahim Fuad <creativeibi77@gmail.com>
2025-06-09 16:12:08 +1000

    nix and basic poem api

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A project comparing a couple of http API options";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.devenv.flakeModule];
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem = {pkgs, ...}: {
+        devenv.shells.default = {
+          devenv.root = let
+              root = builtins.readFile inputs.devenv-root.outPath;
+            in
+              pkgs.lib.mkIf (root != "") root;
+
+          languages = {
+            rust = {
+              enable = true;
+              rustflags = "--cfg tokio_unstable";
+            };
+
+            python = {
+              enable = true;
+              package = pkgs.python313;
+              uv.enable = true;
+            };
+          };
+
+          services = {
+            postgres = {
+              enable = true;
+            };
+
+            opensearch.enable = true;
+          };
+        };
+      };
+    };
+}
